ff0bd9b961c864c4bf550b12ba466590d0a266f4
Anthony Parillo <aparillo87@icloud.com>
2017-09-27 20:54:21 +0300

    Add Lesson 4

---
+++ Lesson_4/Protecting_The_Resume.js
@@ -0,0 +1,20 @@
+var html = '<script src="http://hackyourwebsite.com/eviljavascript.js"></script>';
+
+var charEscape = function(_html) {
+    var newHTML = _html;
+    // How will you make sure that newHTML doesn't contain any < or > ?
+    // Your code goes here!
+
+    //Passes but not simple or correct in the end
+    newHTML = _html.replace("<", "&lt;").replace(">", "&gt;");
+
+ 	//Given example and sets global (g) and shorter
+//     newHTML = _html.replace(/</g, "&lt;");
+//     newHTML = newHTML.replace(/>/g, "&gt;");
+
+    // Don't delete this line!
+    return newHTML;
+};
+
+// Did your code work? The line below will tell you!
+console.log(charEscape(html));
